36e943fc8cece252c66221bf90768ec3a7e68e76
Raghu K <raghuk.vit@gmail.com>
2023-05-08 07:13:51 +0530

    Update aws-ssm-paramter-store/

---
+++ env-dev/main.tfvars
@@ -0,0 +1,4 @@
+parameters = [
+  { name = "dev.frontend.catalogue_url", value = "http://catalogue-dev.rdevopsb72.online:8080/" },
+  { name = "dev.frontend.cart_url", value = "http://cart-dev.rdevopsb72.online:8080/" }
+]
--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-resource "aws_ssm_parameter" "parameter" {
-  name   = "test.test1"
-  value  = "Hello World"
-  type   = "SecureString"
+resource "aws_ssm_parameter" "parameters" {
+  count  = length(var.parameters)
+  name   = var.parameters[count.index].name
+  value  = var.parameters[count.index].value
+  type   = "String"
   key_id = "dce90622-5a23-4f82-a639-be841f534702"
 }
 
