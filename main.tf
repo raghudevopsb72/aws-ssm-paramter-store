aac038d17021d9fcbaa61062288109f21f78c858
Raghu K <raghuk.vit@gmail.com>
2023-05-08 07:09:18 +0530

    Update aws-ssm-paramter-store/

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  type  = "String"
-  value = "Hello World"
+  name   = "test.test1"
+  value  = "Hello World"
+  type   = "SecureString"
+  key_id = "dce90622-5a23-4f82-a639-be841f534702"
 }
 
