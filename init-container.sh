208d53107fc20e564cd406f1228e9a8572bdc7f4
Raghu K <raghuk.vit@gmail.com>
2023-06-29 07:15:46 +0530

    Update aws-ssm-paramter-store/

--- init-container.sh
+++ init-container.sh
@@ -2,6 +2,7 @@ for param in ${PARAMS}; do
   VAR=$(echo $param | awk -F . '{print $NF}' | tr '[:lower:]' '[:upper:]' )
   VAL=$(aws ssm get-parameters  --names $param | jq .Parameters[].Value | sed -e 's/"//g')
   echo $VAR=$VAL
+  echo $VAR=$VAL >>/params/params
 done
 
 
